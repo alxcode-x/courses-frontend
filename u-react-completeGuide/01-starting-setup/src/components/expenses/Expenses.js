e2a7a583000a92de998a02eb5858d686e969c178
Alexis Lopez <alexis.sbtg@gmail.com>
2022-05-16 14:27:36 -0500

    Completed assignment 2. Section 4 completed

--- u-react-completeGuide/01-starting-setup/src/components/expenses/Expenses.js
+++ u-react-completeGuide/01-starting-setup/src/components/expenses/Expenses.js
@@ -1,15 +1,18 @@
 import './Expenses.css';
-import ExpenseItem from "./items/ExpenseItem";
 import Card from '../common/Card';
+import ExpenseItem from "./items/ExpenseItem";
+import ExpensesFilter from './filter/ExpensesFilter';
 
 function items(props) {
-    
+    const filterHandler = (selectedYear) => console.log(selectedYear);
+
     return (
         <Card className="expenses">
-            <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} ></ExpenseItem>
-            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} ></ExpenseItem>
-            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} ></ExpenseItem>
-            <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} ></ExpenseItem>
+            <ExpensesFilter onSelectFilter={filterHandler}/>
+            <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
+            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
+            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
+            <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
         </Card>
     );
 }
