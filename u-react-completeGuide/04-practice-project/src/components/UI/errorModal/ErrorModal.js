3092c0a7976e22a071d81e55794fb27969ea35d7
Alexis Lopez <alexis.sbtg@gmail.com>
2022-11-16 18:32:58 -0600

    Lesson 107. Completed section 9.

--- u-react-completeGuide/04-practice-project/src/components/UI/errorModal/ErrorModal.js
+++ u-react-completeGuide/04-practice-project/src/components/UI/errorModal/ErrorModal.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom'
 import Card from '../card/Card';
 import Button from '../button/Button';
 import styles from './ErrorModal.module.css';
 
+const Backdrop = (props) => {
+    return <div className={styles.backdrop} onClick={props.onConfirm}></div>
+}
+
+const ModalOverlay = (props) => {
+    return (
+        <Card className={styles.modal}>
+            <header className={styles.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <main className={styles.content}>
+                <p>{props.message}</p>
+            </main>
+            <footer className={styles.footer}>
+                <Button onClick={props.onConfirm}>Ok</Button>
+            </footer>
+        </Card>
+    )
+}
+
 function ErrorModal(props) {
     return (
-        <div>
-            <div className={styles.backdrop} onClick={props.onConfirm}></div>
-            <Card className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <main className={styles.content}>
-                    <p>{props.message}</p>
-                </main>
-                <footer className={styles.footer}>
-                    <Button onClick={props.onConfirm}>Ok</Button>
-                </footer>
-            </Card>
-        </div>
+        <Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onConfirm={props.onConfirm} />,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onConfirm={props.onConfirm}
+                />,
+                document.getElementById('overlay-root')
+            )}
+        </Fragment>
     )
 }
 
--- u-react-completeGuide/04-practice-project/src/components/users/addUsers/AddUsers.js
+++ u-react-completeGuide/04-practice-project/src/components/users/addUsers/AddUsers.js
@@ -1,32 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Button from '../../UI/button/Button';
 import styles from './AddUsers.module.css';
 import ErrorModal from '../../UI/errorModal/ErrorModal'
 
 function AddUsers(props) {
-    const [usernameText, setUsernameText] = useState('');
-    const [ageText, setAgeText] = useState('');
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+    // const [usernameText, setUsernameText] = useState('');
+    // const [ageText, setAgeText] = useState('');
     const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if (usernameText.trim().length === 0 || ageText.trim().length === 0) {
+        
+        const enteredName = nameInputRef.current.value;
+        const enteredAge = ageInputRef.current.value;
+
+        if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
             setError({
                 title: 'Invalid input',
                 message: 'Please enter valid data (non-empty values).'
             });
             return;
         }
-        if (+ageText < 1) {
+        if (+enteredAge < 1) {
             setError({
                 title: 'Invalid age',
                 message: 'Please enter a valid age (> 0).'
             }); 
             return;
         }
-        props.onAddUser({ name: usernameText, age: ageText });
-        setUsernameText('');
-        setAgeText('');
+        props.onAddUser({ name: enteredName, age: enteredAge });
+        nameInputRef.current.value = ''; // here we are manipalting the DOM without React 
+        ageInputRef.current.value = '';
+        // setUsernameText('');
+        // setAgeText('');
     }
 
     const errorHandler = () => {
@@ -40,11 +48,11 @@ function AddUsers(props) {
                 <form onSubmit={addUserHandler}>
                     <div className={styles["fields"]}>
                         <label htmlFor="userName">Username</label>
-                        <input type="text" id="userName" onChange={(event) => setUsernameText(event.target.value)} value={usernameText} />
+                        <input type="text" id="userName" ref={nameInputRef}/>
                     </div>
                     <div className={styles.fields}>
                         <label htmlFor="age">Age (Years)</label>
-                        <input type="text" id="age" onChange={(event) => setAgeText(event.target.value)} value={ageText} />
+                         <input type="text" id="age" ref={ageInputRef} />
                     </div>
                     <div className={styles["button-container"]}>
                         <Button className={styles.button} type="submit">Add User</Button>
@@ -57,3 +65,7 @@ function AddUsers(props) {
 }
 
 export default AddUsers;
+
+// === using useState ===
+// line 45: <input type="text" id="userName" onChange={(event) => setUsernameText(event.target.value)} value={usernameText} />
+// line 49: <input type="text" id="age" onChange={(event) => setAgeText(event.target.value)} value={ageText} />
