6614216f35776adf93a1b3c48f631362419d3768
Alexis Lopez <alexis.sbtg@gmail.com>
2022-12-18 17:34:50 -0600

    Lesson 115.

--- u-react-completeGuide/05-side-effects/src/components/Login/Login.js
+++ u-react-completeGuide/05-side-effects/src/components/Login/Login.js
@@ -1,43 +1,61 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (state, action) => {
+  if(action.type === 'USER_INPUT') {
+    return {value: action.val, isValid: action.val.includes('@') };
+  }
+  if(action.type === 'INPUT_BLUR'){
+    return { value: state.value, isValid: state.value.includes('@') }
+  }
+  return { value: '', isValid: false };
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(() => {
-    // it will wait for 500 ms after stop typing to execute setFormIsValid to avoid execute many times.
-    // it waits because of dependencies, every keystroke waits for 500 ms, if we type fast, the timer starts again and there's no time to execute the function
-    // we are using de 'Debouncing' technique here.
-    const identifier = setTimeout(() => {
-      console.log('Checking form validity');
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      );
-    }, 500);
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '', 
+    isValid: false,
+  });
 
-    return () => {
-      clearTimeout(identifier);
-      console.log('CLEAN UP');
-    }
-  }, [enteredEmail, enteredPassword]);
+  // useEffect(() => {
+  //   // it will wait for 500 ms after stop typing to execute setFormIsValid to avoid execute many times.
+  //   // it waits because of dependencies, every keystroke waits for 500 ms, if we type fast, the timer starts again and there's no time to execute the function
+  //   // we are using de 'Debouncing' technique here.
+  //   const identifier = setTimeout(() => {
+  //     console.log('Checking form validity');
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
+
+  //   return () => {
+  //     clearTimeout(identifier);
+  //     console.log('CLEAN UP');
+  //   }
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({type: 'USER_INPUT', val: event.target.value});
+    setFormIsValid(emailState.isValid && enteredPassword.trim().length > 6);
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+    setFormIsValid(emailState.isValid && enteredPassword.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    //setEmailIsValid(emailState.isValid);
+    dispatchEmail({ type: 'INPUT_BLUR'})
   };
 
   const validatePasswordHandler = () => {
@@ -46,7 +64,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -54,14 +72,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
