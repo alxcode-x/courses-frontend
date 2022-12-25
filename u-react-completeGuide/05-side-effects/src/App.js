dc2d9606f5ccc85bf6316019503cb928a7f1c473
Alexis Lopez <alexis.sbtg@gmail.com>
2022-12-24 19:37:07 -0600

    Lesson 121. React Context

--- u-react-completeGuide/05-side-effects/src/App.js
+++ u-react-completeGuide/05-side-effects/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -25,13 +26,15 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    //<React.Fragment>
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {isLoggedIn && <Home onLogout={logoutHandler} />}
         {!isLoggedIn && <Login onLogin={loginHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
+    //</React.Fragment>
   );
 }
 
