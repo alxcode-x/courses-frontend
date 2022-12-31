5e8d5bc65b65fa83dab7b28ad94977d2795b12d1
Alexis Lopez <alexis.sbtg@gmail.com>
2022-12-31 15:49:36 -0600

    Lesson 129. Section 10 completed.

--- u-react-completeGuide/05-side-effects/src/components/Login/Login.js
+++ u-react-completeGuide/05-side-effects/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useReducer, useContext } from 'react';
+import React, { useEffect, useState, useReducer, useContext, useRef } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -40,6 +40,9 @@ const Login = (props) => {
     isValid: null
   });
 
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
 
@@ -78,13 +81,20 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    ctx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      ctx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passworInputdRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailRef}
           id={"email"}
           label={"E-mail"}
           type={"email"}
@@ -94,6 +104,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordRef}
           id={"password"}
           label={"Password"}
           type={"password"}
@@ -103,7 +114,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
