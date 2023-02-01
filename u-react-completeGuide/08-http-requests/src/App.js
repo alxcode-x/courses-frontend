29e33f7760921e5e38e4f1ad676177f3c4ccd72c
Alexis Lopez <alexis.sbtg@gmail.com>
2023-01-31 22:39:06 -0600

    Lesson 179

--- u-react-completeGuide/08-http-requests/src/App.js
+++ u-react-completeGuide/08-http-requests/src/App.js
@@ -1,31 +1,67 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [errors, setErrors] = useState(false);
+  const [isLoading, setIsLoading] = useState(null);
+
+  const fetchMoviesHandler = async () => {
+    setErrors(null);
+    setIsLoading(true);
+    // === Method 1: Promises
+    // fetch('https://swapi.dev/api/films')
+    //   .then(response => { return response.json() })
+    //   .then(data => {
+    //     const moviesData = data.results.map(movie => {
+    //       return {
+    //         id: movie.episode_id,
+    //         title: movie.title,
+    //         openingText: movie.opening_crawl,
+    //         releaseDate: movie.release_date
+    //       }
+    //     })
+    //     setMovies(moviesData);
+    //   })
+    //   .catch(error => setErrors(`Something went wrong. Error: ${error}`));
+
+    // === Method 2: Async call
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+      if (!response.ok) {
+        throw new Error(`Something went wrong. Status code: ${response.status}`)
+      }
+      const data = await response.json();
+      const moviesData = data.results.map(movie => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date
+        }
+      });
+      setMovies(moviesData);
+    }
+    catch (error) {
+      setErrors(error.message);
+    }
+    setIsLoading(false);
+  }
+
+  let content = !isLoading && movies.length > 0 ? <MoviesList movies={movies} /> :
+    !isLoading && movies.length === 0 && !errors ? <p>No movies found.</p> :
+    !isLoading && errors ? <p>{errors}</p> :
+    <p>Loading...</p>
+
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {content}
       </section>
     </React.Fragment>
   );
