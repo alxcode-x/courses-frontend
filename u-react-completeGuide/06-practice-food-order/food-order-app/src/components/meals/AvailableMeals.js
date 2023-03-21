25be33250225d0ddc6034eb901fe8ec4ae8b4e8c
Alexis Lopez <alexis.sbtg@gmail.com>
2023-03-20 22:07:36 -0600

    Lesson 215

--- u-react-completeGuide/06-practice-food-order/food-order-app/src/components/meals/AvailableMeals.js
+++ u-react-completeGuide/06-practice-food-order/food-order-app/src/components/meals/AvailableMeals.js
@@ -1,49 +1,34 @@
 import React from 'react'
-
+import useHttpRequest from '../../hooks/useHttpRequest';
 import classes from './AvailableMeals.module.css'
 import Card from '../common/card/Card';
 import MealItem from './meal-item/MealItem';
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
-
 function AvailableMeal() {
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price}></MealItem>);
+    const { data } = useHttpRequest('https://react-http-test-8f2c7-default-rtdb.firebaseio.com/meals.json', null);
+
+    let mealsList = [];
+
+    for (const key in data) {
+        mealsList.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+        });
+    }
+
+    const meals = mealsList.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price}></MealItem>);
 
     return (
         <section className={classes.meals}>
             <Card>
                 <ul>
-                    {mealsList}
+                    {meals}
                 </ul>
             </Card>
-
         </section>
     )
 }
 
-export default AvailableMeal
+export default AvailableMeal;
