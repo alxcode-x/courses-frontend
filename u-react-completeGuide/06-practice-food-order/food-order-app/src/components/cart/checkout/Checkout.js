95b0c0964f64ee7836e277663a063a5061325605
Alexis Lopez <alexis.sbtg@gmail.com>
2023-03-26 15:38:29 -0600

    Lesson 220 -not completed, pending add useReducer instead useState-

--- u-react-completeGuide/06-practice-food-order/food-order-app/src/components/cart/checkout/Checkout.js
+++ u-react-completeGuide/06-practice-food-order/food-order-app/src/components/cart/checkout/Checkout.js
@@ -1,33 +1,57 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import classes from './Checkout.module.css';
 
+const isEmpty = (value) => value.trim().length === 0;
+const isFiveChars = (value) => value.trim().length >= 5;
+
 function Checkout(props) {
-    const submitHandler = (event) => {
+    const nameRef = useRef();
+    const streetRef = useRef();
+    const postalRef = useRef();
+    const cityRef = useRef();
+
+    const confirmHandler = (event) => {
         event.preventDefault();
-    }
+
+        const nameIsValid = !isEmpty(nameRef.current.value) && isFiveChars(nameRef.current.value);
+        const streetIsValid = !isEmpty(streetRef.current.value) && isFiveChars(streetRef.current.value);
+        const postalIsValid = !isEmpty(postalRef.current.value) && isFiveChars(postalRef.current.value);
+        const cityIsValid = !isEmpty(cityRef.current.value) && isFiveChars(cityRef.current.value);
+
+        const formIsValid = nameIsValid && streetIsValid && postalIsValid && cityIsValid;
+
+        if(!formIsValid){
+            return;
+        }
+
+    };
 
     return (
-        <form onSubmit={submitHandler}>
+        <form className={classes.form} onSubmit={confirmHandler}>
             <div className={classes.control}>
-                <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" />
+                <label htmlFor='name'>Your Name</label>
+                <input type='text' id='name' ref={nameRef} />
             </div>
             <div className={classes.control}>
-                <label htmlFor="street">Street</label>
-                <input type="text" id="street" />
+                <label htmlFor='street'>Street</label>
+                <input type='text' id='street' ref={streetRef} />
             </div>
             <div className={classes.control}>
-                <label htmlFor="postal">Postal Code</label>
-                <input type="text" id="postal" />
+                <label htmlFor='postal'>Postal Code</label>
+                <input type='text' id='postal' ref={postalRef} />
             </div>
             <div className={classes.control}>
-                <label htmlFor="city">City</label>
-                <input type="text" id="city" />
+                <label htmlFor='city'>City</label>
+                <input type='text' id='city' ref={cityRef} />
             </div>
-            <button type="button" onClick={props.onCancel}>Cancel</button>
-            <button type="submit" onClick={submitHandler}>Confirm</button>
+            <div className={classes.actions}>
+                <button type='button' onClick={props.onCancel}>
+                    Cancel
+                </button>
+                <button className={classes.submit}>Confirm</button>
+            </div>
         </form>
-    )
+    );
 }
 
 export default Checkout
