0d1fcda0fe3c408d0c01e80c5f17e6b388d00e1b
Alexis Lopez <alexis.sbtg@gmail.com>
2023-06-28 22:54:29 -0600

    Lesson 340

---
+++ u-react-completeGuide/12-routing/practice/frontend/src/components/NewsletterSignup.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useFetcher } from 'react-router-dom';
+import classes from './NewsletterSignup.module.css';
+
+function NewsletterSignup() {
+  const {data, state} = useFetcher();
+
+  useEffect(() => {
+    if(state === 'idle' && data && data.message){
+      window.alert(data.message);
+    }
+  }, [data, state]);
+  return (
+    <form method="post" className={classes.newsletter}>
+      <input
+        type="email"
+        placeholder="Sign up for newsletter..."
+        aria-label="Sign up for newsletter"
+      />
+      <button>Sign up</button>
+    </form>
+  );
+}
+
+export default NewsletterSignup;
