c5531e08854c3f50605d405d38411160cfe8fd82
Alexis Lopez <alexis.sbtg@gmail.com>
2023-06-28 22:52:30 -0600

    Lesson 340

--- u-react-completeGuide/12-routing/practice/frontend/src/pages/EventDetailPage.js
+++ u-react-completeGuide/12-routing/practice/frontend/src/pages/EventDetailPage.js
@@ -1,36 +1,65 @@
-import React from 'react'
-import { useRouteLoaderData, json, redirect } from 'react-router-dom'
+import React, { Suspense } from 'react'
+import { useRouteLoaderData, json, redirect, defer, Await } from 'react-router-dom'
 import EventItem from '../components/EventItem';
+import EventsList from '../components/EventsList';
 
 function EventDetailPage() {
-  const data = useRouteLoaderData('event-detail');
+  const { event, events } = useRouteLoaderData('event-detail');
 
   return (
     <>
-      <EventItem event={data}/>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
     </>
   )
 }
 
 export default EventDetailPage
 
-export async function eventDetailsLoader({request, params}) {
-  const response = await fetch(`http//:localhost:8080/events/${params.id}`);
-  if(response.ok){
-    return response;
+const loadEvents = async () => {
+  const response = await fetch('http://localhost:8080/events1');
+  if (response.ok) {
+    const data = await response.json();
+    return data.events;
   }
   else {
-    throw json({message: 'Could not fetch event details.'}, {status: 500});
+    throw new Response(JSON.stringify({ message: "No events found!" }), { status: 400 });
   }
 }
 
-export async function deleteAction({params, request}) {
+const loadEvent = async (id) => {
+  const response = await fetch(`http//:localhost:8080/events/${id}`);
+  if (response.ok) {
+    const data = response.json();
+    return data.event;
+  }
+  else {
+    throw json({ message: 'Could not fetch event details.' }, { status: 500 });
+  }
+}
+
+export async function eventDetailsLoader({ request, params }) {
+  defer({
+    event: await loadEvent(params.id), //usign 'await' here, we can controll if loader will run before or after render the component. Here, the component will be rendered until loadEvents data is loaded.
+    events: loadEvents(), // Here, without 'await', component will be rendered first and then the loaded will be executed.
+  });
+}
+
+export async function deleteAction({ params, request }) {
   const response = fetch(`http://localhost:8080/events ${params.eventId}`, {
     method: request.method,
   });
 
-  if(!response.ok){
-    throw json({message: 'Could not delete event.'}, {status: 500});
+  if (!response.ok) {
+    throw json({ message: 'Could not delete event.' }, { status: 500 });
   }
 
   return redirect('/events');
--- u-react-completeGuide/12-routing/practice/frontend/src/pages/EventsPage.js
+++ u-react-completeGuide/12-routing/practice/frontend/src/pages/EventsPage.js
@@ -1,26 +1,48 @@
-import { useLoaderData } from 'react-router-dom';
+import { Suspense } from 'react';
+import { useLoaderData, defer, Await } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
+  // Await will wait until events is resolved, then will execute the function to render EventsList.
   return (
-    <>
-      <EventsList events={data.events} />
-    </>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
+
+  // const data = useLoaderData();
+  // return (
+  //   <>
+  //     <EventsList events={data.events} />
+  //   </>
+  // );
 }
 
-const eventsLoader = async () => {
+const loadEvents = async () => {
   const response = await fetch('http://localhost:8080/events1');
   if (response.ok) {
-    // const data = await response.json(); // since useLoaderDate retuns a promise that resolves a Request(), it's not necessary manage the response here.
+    // [1] const data = await response.json(); // since useLoaderDate retuns a promise that resolves a Request(), it's not necessary manage the response here.
     // return data;
-    return response;
+    
+    //[2] return response;
+
+    //since we are using 'defer' we can't return the promise to be awaited by react-router-dom, instead we have to manually parse the response.
+    const data = await response.json();
+    return data.events;
   }
   else {
     throw new Response(JSON.stringify({ message: "No events found!" }), { status: 400 }); // by throwing an exception, the closest error page to this component, will be rendered
   }
+}
+
+const eventsLoader = async () => {
+  return defer({
+    events: loadEvents(),
+  });
 }
 
 export default EventsPage;
