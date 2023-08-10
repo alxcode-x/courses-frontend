a556b249273c2beeee7125ec9c14438456b07760
Alexis Lopez <alexis.sbtg@gmail.com>
2023-08-09 22:41:21 -0600

    Lesson 387.

--- u-react-completeGuide/15-next-js/practice-project/pages/[meetupId]/index.js
+++ u-react-completeGuide/15-next-js/practice-project/pages/[meetupId]/index.js
@@ -1,1 +1,14 @@
 //domain.com/meetupId
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetailsPage (){
+    return (
+        <MeetupDetail 
+            image="https://www.google.com/url?sa=i&url=https%3A%2F%2Fopidesign.net%2Flandscape-architecture%2Flandscape-architecture-fun-facts%2F&psig=AOvVaw2pPyuCUMyWaLlwFgIgpwIS&ust=1691718770648000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLiPw9X90IADFQAAAAAdAAAAABAE"
+            title="first meetup"
+            address="some address"
+            description="some description" />
+    )
+}
+
+export default MeetupDetailsPage;
--- u-react-completeGuide/15-next-js/practice-project/pages/index.js
+++ u-react-completeGuide/15-next-js/practice-project/pages/index.js
@@ -1,13 +1,18 @@
 //domain.com
 import MeetupList from '../components/meetups/MeetupList';
 
-function HomePage() {
-    return (
-        <>
-            <MeetupLis />
-        </>
-    )
-
+function HomePage(props) {
+    return <MeetupLis meetups={props.meetups}/>
 };
+
+export async function getStaticProps() { // this is execute in build to be ready when the component is loaded.
+    return {
+        props: { //this props are the props teh component receives
+            meetups: {
+                // something
+            }
+        }
+    }
+}
 
 export default HomePage;
