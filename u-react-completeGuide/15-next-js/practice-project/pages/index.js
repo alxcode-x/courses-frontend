c1c1bd43c9e9940bbe74c1e118da0f7c7de91e24
Alexis Lopez <alexis.sbtg@gmail.com>
2023-08-16 20:46:33 -0600

    Lesson 389

--- u-react-completeGuide/15-next-js/practice-project/pages/index.js
+++ u-react-completeGuide/15-next-js/practice-project/pages/index.js
@@ -5,14 +5,30 @@ function HomePage(props) {
     return <MeetupLis meetups={props.meetups}/>
 };
 
-export async function getStaticProps() { // this is execute in build to be ready when the component is loaded.
+// this is execute in build to be ready when the component is loaded, and every time is loaded again, it will be ready with the same data.
+// better use if data is static or rarely changes.
+export async function getStaticProps() { 
     return {
         props: { //this props are the props teh component receives
             meetups: {
                 // something
             }
+        },
+        revalidate: 10 // this will rexecute the function every 10 seconds after deployment. Use only if data is constantly changing.
+    };
+};
+
+// this is executed in the server after deployment and every time the component is requested. This ensure the data is up to date.
+// better use if data constantly changes.
+export async function getServerSideProps(context) {
+    const req = context.req;
+    const res = context.res;
+
+    return {
+        props: {
+            //something
         }
-    }
-}
+    };
+};
 
 export default HomePage;
