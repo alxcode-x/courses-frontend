b07be0eb0851403740415f80d8e567318122e766
Alexis Lopez <alexis.sbtg@gmail.com>
2023-09-05 22:55:31 -0600

    Lesson 413.

--- u-react-completeGuide/15-next-js/practice-project/pages/api/new-meetups.js
+++ u-react-completeGuide/15-next-js/practice-project/pages/api/new-meetups.js
@@ -1,8 +1,21 @@
 // POST /api/new-meetup
+import { MongoClient } from 'mongodb';
 
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
+
+        const client = await MongoClient.connect("mongodb+srv://<username>:<password>@cluster0.kvz0sgn.mongodb.net/<collection>?retryWrites=true&w=majority"); //replace <> values
+        const db = client.db();
+
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data); //this receives an object
+
+        console.log(result);
+
+        client.close();
+
+        res.status(201).json({message: 'Meetup inserted!'});
     }
 }
 
