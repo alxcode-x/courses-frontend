a54bef7a849b41c9e981cb1e27d7bb719854aebb
Alexis Lopez <alexis.sbtg@gmail.com>
2023-09-06 22:31:38 -0600

    Lesson 414

--- u-react-completeGuide/15-next-js/practice-project/pages/new-meetup/index.js
+++ u-react-completeGuide/15-next-js/practice-project/pages/new-meetup/index.js
@@ -1,9 +1,21 @@
 //domain.com/new-meetup
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-    const addMeetupHandler = (enteredMeetupData) => {
-        console.log(enteredMeetupData);
+    const router = useRouter();
+
+    const addMeetupHandler = async (enteredMeetupData) => {
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(enteredMeetupData),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+
+        await response.json();
+        router.push('/');
     };
 
     return <NewMeetupForm onAddMeetup={addMeetupHandler} />
