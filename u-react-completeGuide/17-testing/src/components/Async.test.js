5f00ce37d3e28bfff146193d39abb19a07794530
Alexis Lopez <alexis.sbtg@gmail.com>
2024-02-21 22:05:43 -0600

    completed Section 29.

--- u-react-completeGuide/17-testing/src/components/Async.test.js
+++ u-react-completeGuide/17-testing/src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from "./Async";
 
 describe("Async component", () => {
     test("Render posts if succeeds", async () => {
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id: 'p1', title: 'First post'}],
+        });
         render(<Async />);
 
         const elements = await screen.findAllByRole("listitem");
